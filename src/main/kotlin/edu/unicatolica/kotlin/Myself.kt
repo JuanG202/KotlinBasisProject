499ca2f7252b5ce17400ba13ef00ec9f3853ebbc
Juan Granja <4684593@gmail.com>
2025-08-14 14:01:14 -0500

    se agrega archivo myself con informacion sobre mi

---
+++ src/main/kotlin/edu/unicatolica/kotlin/Myself.kt
@@ -0,0 +1,13 @@
+package edu.unicatolica.kotlin
+
+fun main(){
+
+    val nombre = "Juan Pablo Granja"
+    val edad: Int = 23
+    val  programa = "Desarrollo soft"
+    val  interes = "Amor a la tecnologia"
+
+    val message = "Hola, soy $nombre tengo $edad años y estudio actualmente $programa por puro $interes"
+    println(message)
+
+}
